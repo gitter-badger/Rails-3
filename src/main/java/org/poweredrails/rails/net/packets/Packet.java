dd8860d4d2f324f4ca86074a7668b1044ad9b54f
Dylan Curzon <d.46.curzon@hotmail.co.uk>
2015-09-13 03:00:07 +0100

    Change format to make checkstyle happy

--- src/main/java/org/poweredrails/rails/net/packets/Packet.java
+++ src/main/java/org/poweredrails/rails/net/packets/Packet.java
@@ -1,35 +1,58 @@
+/*
+ * This file is a part of the multiplayer platform Powered Rails, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) Powered Rails
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package org.poweredrails.rails.net.packets;
 
-import io.netty.buffer.ByteBuf;
 import org.poweredrails.rails.net.handlers.HandlerRegistry;
 
 public interface Packet {
 
     /**
      * <p>
-     *     ...
+     *     Read the packet data from the buffer.
      * </p>
      *
-     * @param buf
+     * @param buf Buffer class to read from.
      */
-    public void fromBuffer(Buffer buf);
+    void fromBuffer(Buffer buf);
 
     /**
      * <p>
-     *     ...
+     *     Write the packet data to a new buffer and return it.
      * </p>
      *
-     * @return
+     * @return New buffer containing serialized data.
      */
-    public Buffer toBuffer();
+    Buffer toBuffer();
 
     /**
      * <p>
-     *     ...
+     *     Handle the packet.
      * </p>
      *
-     * @param registry
+     * @param registry Instance of handler registry.
      */
-    public void handle(HandlerRegistry registry);
+    void handle(HandlerRegistry registry);
 
 }

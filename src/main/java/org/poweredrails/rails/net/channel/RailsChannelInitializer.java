fa143b9b0e73f396b064c8780ccb895167310689
Vectrix <vectrixu@gmail.com>
2015-09-13 17:44:08 +1200

    Beginning ChannelInitializer.

---
+++ src/main/java/org/poweredrails/rails/net/channel/RailsChannelInitializer.java
@@ -0,0 +1,14 @@
+package org.poweredrails.rails.net.channel;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.socket.SocketChannel;
+
+public class RailsChannelInitializer extends ChannelInitializer<SocketChannel> {
+
+    @Override
+    protected final void initChannel(SocketChannel socketChannel) {
+
+    }
+
+
+}
